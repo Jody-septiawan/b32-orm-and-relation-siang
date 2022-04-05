778d57e9d4a0b4c44fe69cce7ff26fcf051b73dc
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-05 13:36:14 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,13 @@
 // import model here
+const { user } = require('../../models');
 
 exports.addUsers = async (req, res) => {
-    // code here
-}
+  const data = req.body;
+
+  await user.create(data);
+
+  res.send({
+    status: 'success',
+    message: 'Insert user data finished!',
+  });
+};
